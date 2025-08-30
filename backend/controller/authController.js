6852fc0477cdaaafd2ccc7aa3f2aa99496a139fe
Muhammad-Hammad <hammaddeveloper02@gmail.com>
2025-08-30 13:58:17 +0500

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -31,8 +31,8 @@ export const registration = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -66,8 +66,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -107,8 +107,8 @@ try {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
